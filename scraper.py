1092d5ffd4eb9f4a789a907f2ff1ccaf677ffcc5
Daft <voglauer.daniel@gmail.com>
2026-02-14 19:29:11 +0100

    added continuous scraping

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone, timedelta
+
 from dotenv import load_dotenv
 
 from github import get_files, search_repos
@@ -8,65 +10,71 @@ from math import ceil
 import time
 from util import log
 
-query = "stars:<2 language:Python language:JavaScript language:TypeScript created:2026-02-14T16:50:00Z..2026-02-14T17:00:00Z"
-
 size = 15
-
-count = search_repos(query)["total_count"]
-log(f"Found {count} results...")
 
 searched = []
 
-for p in range(1, ceil(count/size) + 1):
-    log(f"Loading Page {p}/{ceil(count/size) + 1}")
-    page = search_repos(query, p, size)
+while True:
+    now = datetime.now(timezone.utc) - timedelta(minutes=1)
+    before = datetime.now(timezone.utc) - timedelta(minutes=2)
+
+    query = f"stars:<2 language:Python language:JavaScript language:TypeScript created:{before.strftime("%Y-%m-%dT%H:%M:%SZ")}..{now.strftime("%Y-%m-%dT%H:%M:%SZ")}"
+
+    log(f"Querying \"{query}\"")
+
+    count = search_repos(query)["total_count"]
+    log(f"Found {count} matching repositories...")
+
+    for p in range(1, ceil(count/size) + 1):
+        log(f"Loading Page {p}/{ceil(count/size)}")
+        page = search_repos(query, p, size)
 
 
 
-    for repo in page["items"]:
-        time.sleep(1)
+        for repo in page["items"]:
+            time.sleep(1)
 
-        name = repo["full_name"]
-        branch = repo.get("default_branch", "main")
-        language = repo.get("language")
+            name = repo["full_name"]
+            branch = repo.get("default_branch", "main")
+            language = repo.get("language")
 
-        if name in searched:
-            log("Already searched")
-            continue
-
-        searched.append(name)
-
-
-        tmp = True
-        while tmp:
-            log(f"Scraping {name}...  ".ljust(70))
-
-            try:
-                files = get_files(name)
-            except:
-                log("Failed to fetch Files","error")
-                time.sleep(5)
+            if name in searched:
+                log("Already searched")
                 continue
 
-            tmp = False
+            searched.append(name)
 
-        secrets = []
 
-        for file in files:
-            path: str = file["path"]
+            tmp = True
+            while tmp:
+                log(f"Scraping {name}...  ".ljust(70))
 
-            if file["type"] == "tree": continue
-            if file.get("size", 0) == 0: continue
+                try:
+                    files = get_files(name)
+                except:
+                    log("Failed to fetch Files","error")
+                    time.sleep(5)
+                    continue
 
-            if ".env" in path and not "example" in path:
-                secrets.append(file)
+                tmp = False
 
-        if len(secrets) == 0:
-            continue
+            secrets = []
 
-        with open("secrets.csv", "a") as f:
-            log(f"Found Secrets for {name}", "results")
+            for file in files:
+                path: str = file["path"]
 
-            for sec in secrets:
-                path = sec["path"]
-                f.write(f"{name},{branch},{language},https://github.com/{name}/blob/{branch}/{path},{sec["sha"]}\n")
+                if file["type"] == "tree": continue
+                if file.get("size", 0) == 0: continue
+
+                if ".env" in path and not "example" in path:
+                    secrets.append(file)
+
+            if len(secrets) == 0:
+                continue
+
+            with open("secrets.csv", "a") as f:
+                log(f"Found Secrets for {name}", "results")
+
+                for sec in secrets:
+                    path = sec["path"]
+                    f.write(f"{name},{branch},{language},https://github.com/{name}/blob/{branch}/{path},{sec["sha"]}\n")
